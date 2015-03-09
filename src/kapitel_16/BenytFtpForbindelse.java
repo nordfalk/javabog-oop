1ac545f75e50591f1e9d630fda3630882d7520b1
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2015-03-09 19:38:03 +0100

    Ført ind i oopj455_netværk

--- src/kapitel_16/BenytFtpForbindelse.java
+++ src/kapitel_16/BenytFtpForbindelse.java
@@ -4,12 +4,12 @@ public class BenytFtpForbindelse {
 	{
 		FtpForbindelse f = new FtpForbindelse();
 		// bemærk - vær altid MEGET FORSIGTIG med at angive adgangskoder i en fil!!
-		f.forbind("pingo.cv.ihk.dk","nordfalk","adgangskoden");
+		f.forbind("javabog.dk","nordfalk","adgangskoden");
 
 		f.sendKommando("HELP");    // få liste over kommandoer som tjenesten kender
 		f.modtagTekst("LIST");     // få liste over filer på værten
 
-		String indhold = "Indhold af en lille fil med navnet:\ntil.txt";
+		String indhold = "Indhold af en lille fil med navnet:\nfil.txt\n";
 		f.sendTekst("STOR fil.txt", indhold);       // gem en tekstfil på værten
 
 		indhold = f.modtagTekst("RETR fil.txt");    // hent filen igen 

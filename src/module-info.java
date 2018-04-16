2270b3a4d5ab8483774cec4f5cd82820831a84f6
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2018-04-16 12:10:53 +0200

    Opsætningsfiler til IntelliJ og Netbeans opdateret til Java 9. Appletter fjernet

---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+// Denne fil er beregnet på Java 9.
+// Hvis du vil compile projektet til Java 8 eller tidligere skal du fjerne filen
+
+module dk.javabog.oop {
+    requires java.se;
+    requires java.xml.ws;
+}

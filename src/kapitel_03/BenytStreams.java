1b5d2da5bbdf3a6ceff14453ca88597c10dcd048
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2018-05-02 14:19:37 +0200

    Streams

--- src/kapitel_03/BenytStreams.java
+++ src/kapitel_03/BenytStreams.java
@@ -1,47 +1,44 @@
 package kapitel_03;
 
 import java.util.*;
-import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
-// TODO tjek at ListerJava8 er omdøbt til BenytStreams alle steder
 
 public class BenytStreams {
 
 	public static void main(String[] args) {
-		// I java 9 kan man også skrive: List.of("is", "sko", "bil", "hammer", "tov");
+
 		List<String> liste = Arrays.asList("is", "sko", "bil", "hammer", "tov");
+		// I java 9 kan man også skrive: List.of("is","sko","bil","hammer","tov");
 		System.out.println("Listen indeholder:   " + liste);
 
-		// map(): Definér en funktion der udføres på hvert element som giver et nyt elemebt
-		List<String> sb = liste.stream().map(s -> s.toUpperCase()).collect(Collectors.toList());
-		System.out.println("Listen med store bogstaver: " + sb);
+		Function<String, String> storeBogstFunktion = s -> s.toUpperCase();
 
-		// mapToInt(): Definér en funktion der kører på hvert element og giver en int
+		// map(): Udfør funktion på hvert element for at få et nyt element
+		System.out.println("Listen med store bogstaver: " +
+			liste.stream().map(storeBogstFunktion).collect(Collectors.toList()));
+
+		// mapToInt(): Udfør på hvert element en funktion, der giver en int
 		// sum(): giver summen af alle elementer i en IntStream
 		int antalBogstaver = liste.stream().mapToInt(s -> s.length()).sum();
 		System.out.println("Bogstaver i alt:     " + antalBogstaver);
 
 		// filter(): Filtrér efter et kriterium (her s.length()==3)
-		List<String> lgd3 = liste.stream().filter(s -> s.length() == 3).collect(Collectors.toList());
-		System.out.println("Dem med tre bogstaver: " + lgd3);
+		List<String> lgd3 =
+			liste.stream().filter(s -> s.length() == 3).collect(Collectors.toList());
+		System.out.println("De med tre bogstaver: " + lgd3);
 
 		// parallelStream() - starter flere tråde, der hver behandler et element
 		liste.parallelStream().forEach(element -> {
-			try {
-				System.out.print(element + "1; ");
-				Thread.sleep((int) (2000 * Math.random()));
-				System.out.print(element + "2; ");
-				Thread.sleep((int) (2000 * Math.random()));
-				System.out.print(element + "3; ");
-			} catch (InterruptedException ex) {
-			}
+			System.out.print(element + "1; ");
+			try { Thread.sleep((int) (2000 * Math.random())); } catch (Exception e) {}
+			System.out.print(element + "2; ");
+			try { Thread.sleep((int) (2000 * Math.random())); } catch (Exception e) {}
+			System.out.print(element + "3; ");
 		});
-		System.out.println("\nparallelStream() slut");
+		System.out.println("\nKald til parallelStream() slut");
 	}
 }
-
 
 /*
     // takes a Long, returns a String

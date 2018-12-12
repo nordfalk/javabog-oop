82930bcf9b1b3dd6769780c64b6488746671b9b8
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2018-12-12 20:27:06 +0100

    SOAP-eksempel fjernet - tilhører Java EE. Dermed ophører behovet for at arbejde med moduler, så det er fjernet igen og projektet er nu uden videre kompatibelt med Java 8

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-// BEMÆRK: Denne fil er beregnet på Java 9 og senere.
-//
-// Hvis du vil compile projektet til Java 8 eller tidligere skal du fjerne filen
-
-module dk.javabog.oop {
-    requires java.se;
-		// nedenstående er nødvendig for at kapitel_19_soap i Java 9 og senere
-    requires java.xml.ws;
-}

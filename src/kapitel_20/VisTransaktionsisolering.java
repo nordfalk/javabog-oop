543ed985945065d35fa82f9bf02ba0573b0e1932
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2020-03-31 12:19:05 +0200

    grundigere demo af transaktionsisolering

--- src/kapitel_20/VisTransaktionsisolering.java
+++ src/kapitel_20/VisTransaktionsisolering.java
@@ -4,14 +4,27 @@ import java.sql.*;
 
 public class VisTransaktionsisolering {
 	public static void main(String[] arg) throws Exception {
-		new Thread(() -> læsning()).start();
-		Thread.sleep(1500);
-		new Thread(() -> skriv()).start();
+		kørLæsningOgSkrivning(0);
+		kørLæsningOgSkrivning(Connection.TRANSACTION_READ_UNCOMMITTED);
+		kørLæsningOgSkrivning(Connection.TRANSACTION_READ_COMMITTED);
+		kørLæsningOgSkrivning(Connection.TRANSACTION_REPEATABLE_READ);
+		kørLæsningOgSkrivning(Connection.TRANSACTION_SERIALIZABLE);
 	}
 
-	private static int transaktionsisolering = 0; //Connection.TRANSACTION_SERIALIZABLE;
+	private static void kørLæsningOgSkrivning(int transaktionsisolering) throws Exception {
+		System.out.println(" ********* transaktionsisolering = " + transaktionsisolering + " ****************");
+		Thread læsTråd = new Thread(() -> læsning(transaktionsisolering));
+		Thread skrivTråd = new Thread(() -> skriv(transaktionsisolering));
+		læsTråd.start();
+		Thread.sleep(1500);
+		skrivTråd.start();
+		læsTråd.join();
+		skrivTråd.join();
+		System.out.println("----- alle tråde er nu afsluttet -----\n");
+	}
 
-	private static void læsning() {
+
+	private static void læsning(int transaktionsisolering) {
 		try {
 			Connection forb = DriverManager.getConnection("jdbc:mysql:///test?useSSL=false");
 			if (transaktionsisolering > 0) forb.setTransactionIsolation(transaktionsisolering);
@@ -21,14 +34,11 @@ public class VisTransaktionsisolering {
 
 			for (int i = 0; i < 8; i++) {
 
-				ResultSet rs = stmt.executeQuery("select NAVN, KREDIT from KUNDER");
-				String res = "";
-				while (rs.next()) {
-					String navn = rs.getString("NAVN");
-					double kredit = rs.getDouble("KREDIT");
-					res += navn + " " + kredit + "; ";
+				if (i == 4) {
+					//stmt.executeUpdate("update KUNDER set KREDIT=55555 WHERE navn='Robert'");
 				}
-				System.out.println("Læsning: " + res);
+				String res = læsIndhold(stmt);
+				System.out.println("Læsning fik: '" + res + "'");
 
 				Thread.sleep(1000);
 			}
@@ -40,8 +50,19 @@ public class VisTransaktionsisolering {
 		}
 	}
 
+	private static String læsIndhold(Statement stmt) throws SQLException {
+		ResultSet rs = stmt.executeQuery("select NAVN, KREDIT from KUNDER");
+		String res = "'";
+		while (rs.next()) {
+			String navn = rs.getString("NAVN");
+			double kredit = rs.getDouble("KREDIT");
+			res += navn + " " + kredit + "; ";
+		}
+		return res + "'";
+	}
 
-	private static void skriv() {
+
+	private static void skriv(int transaktionsisolering) {
 		try {
 			String tråd = Thread.currentThread().getName() + "                                      ";
 			Connection forb = DriverManager.getConnection("jdbc:mysql:///test?useSSL=false");
@@ -51,17 +72,17 @@ public class VisTransaktionsisolering {
 			Statement stmt = forb.createStatement();
 
 			stmt.executeUpdate("delete from KUNDER"); // slet alle rækker fra tabellen
-			System.out.println(tråd + " slettet alle rækker. Tabellen er nu tom.");
+			System.out.println(tråd + " slettet alle rækker, så tabellen er tom - " + læsIndhold(stmt));
 			Thread.sleep(1000);
 
 			stmt.executeUpdate("insert into KUNDER values('Jacob', 20)");
 			stmt.executeUpdate("insert into KUNDER values('Robert', 20)");
-			System.out.println(tråd + " indsat Jacob og Robert med 20 kr");
+			System.out.println(tråd + " indsat Jacob og Robert med 20 kr        - " + læsIndhold(stmt));
 			Thread.sleep(1000);
 
-			int robertKredit = 100 + (int) (Math.random() * 100);
+			int robertKredit = 100 + transaktionsisolering; // 100, 101, 102, 104 eller 108
 			stmt.executeUpdate("update KUNDER set KREDIT=" + robertKredit + " WHERE navn='Robert'");
-			System.out.println(tråd + " Robert har nu " + robertKredit + " kr");
+			System.out.println(tråd + " Robert har nu " + robertKredit + " kr        - " + læsIndhold(stmt));
 			Thread.sleep(1000);
 
 			if (transaktionsisolering > 0) forb.commit();
